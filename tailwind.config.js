75e967119eb7be030bdd1946a08b2e5254c84a15
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-05-25 22:27:36 +0700

    [EPM01][LoiLT] feat: config file tailwind.config.js and add default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#F27227",
+          50: "#FCE3D4",
+          100: "#FAC7A9",
+          200: "#F7AA7D",
+          300: "#F69C68",
+          400: "#F3803D",
+          500: "#DA6723",
+          600: "#C25B1F",
+          700: "#A9501B",
+          800: "#914417",
+          900: "#793914",
+        },
+        secondary: "#262525",
+        "light-text-color": "#FFFFFF",
+        "second-text-color": "#737373",
+        "dark-text-color": "#000000",
+      },
+    },
   },
   plugins: [],
 };
