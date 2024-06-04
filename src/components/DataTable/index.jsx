dd85391422cc73cbb971845552880571423920dc
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-06-04 21:54:43 +0700

    [EPM-22][LoiLT] feat: update datatable component

--- src/components/DataTable/index.jsx
+++ src/components/DataTable/index.jsx
@@ -5,8 +5,11 @@ import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Close";
+import { InputAdornment } from "@mui/material";
+import { TextField, IconButton } from "@mui/material";
 import {
   DataGrid,
   GridRowModes,
@@ -73,11 +76,22 @@ function EditToolbar({ setRows, setRowModesModel }) {
   };
 
   return (
-    <>
-      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-        Add record
-      </Button>
-    </>
+    <Button
+      onClick={handleClick}
+      sx={{
+        borderRadius: 1,
+        backgroundColor: "primary.main",
+        color: "text.light",
+        height: 36,
+        width: 73,
+        padding: '10px',
+        fontSize: 12,
+        textTransform: 'none',
+      }}
+    >
+      Thêm
+      <AddIcon sx={{ color: "text.light", width: 15, height: 15 }}/>
+    </Button>
   );
 }
 
@@ -127,36 +141,40 @@ function DataTable() {
 
   //configure columns
   const columns = [
-    { field: "name", headerName: "Name", width: 180, editable: true },
     {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 80,
+      field: "id",
+      headerName: "ID",
+      headerClassName: "header",
+      type: "string",
+      width: 137,
+      align: "left",
+      editable: false,
+    },
+    {
+      field: "name",
+      headerName: "Tên",
+      headerClassName: "header",
+      type: "string",
+      width: 539,
       align: "left",
       headerAlign: "left",
+
       editable: true,
     },
     {
-      field: "joinDate",
-      headerName: "Join date",
-      type: "date",
-      width: 180,
+      field: "email",
+      headerName: "Email",
+      headerClassName: "header",
+      type: "string",
+      width: 560,
       editable: true,
-    },
-    {
-      field: "role",
-      headerName: "Department",
-      width: 220,
-      editable: true,
-      type: "singleSelect",
-      valueOptions: ["Market", "Finance", "Development"],
     },
     {
       field: "actions",
       type: "actions",
-      headerName: "Actions",
-      width: 100,
+      headerClassName: "header",
+      headerName: "Hành động",
+      width: 137,
       cellClassName: "actions",
       getActions: ({ id }) => {
         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
@@ -199,30 +217,87 @@ function DataTable() {
   ];
 
   return (
-    <Box
-      sx={{
-        height: 500,
-        width: "100%",
-        "& .actions": { color: "text.secondary" },
-        "& .textPrimary": { color: "text.primary" },
-      }}
-    >
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        editMode="row"
-        rowModesModel={rowModesModel}
-        onRowModesModelChange={handleRowModesModelChange}
-        onRowEditStop={handleRowEditStop}
-        processRowUpdate={processRowUpdate}
-        slots={{
-          toolbar: EditToolbar,
-        }}
-        slotProps={{
-          toolbar: { setRows, setRowModesModel },
-        }}
-      />
-    </Box>
+    <>
+      <Box className="py-[24px] px-[20px]">
+        <Box className="flex justify-end w-[1376px] h-[36px] mb-20 gap-x-[24px]">
+          <TextField
+            className="rounded-sm border-2"
+            placeholder="Tìm kiếm"
+            variant="outlined"
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "text.light",
+                },
+                "&:hover fieldset": {
+                  borderColor: "text.light",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "text.light",
+                },
+                color: "text.light",
+              },
+              "& .MuiOutlinedInput-input::placeholder": {
+                color: "text.light",
+                fontSize: 12,
+              },
+              "& .MuiOutlinedInput-input": {
+                padding: "6px",      
+              },
+              width: 247,
+
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton>
+                    <SearchIcon
+                      sx={{ color: "text.light", width: 15, height: 15 }}
+                    />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <EditToolbar setRows={setRows} setRowModesModel={setRowModesModel} />
+        </Box>
+        <Box
+          sx={{
+            height: 650,
+            width: 1376,
+            overflow: "hidden",
+            "& .MuiPaginationItem-page.Mui-selected": {
+              color: "text.light",
+            },
+            "& .actions": { color: "text.light" },
+            "& .textPrimary": { color: "text.light" },
+            "& .header": {
+              backgroundColor: "primary.main",
+              color: "text.light",
+            },
+
+            border: "1px solid",
+            borderRadius: 1,
+          }}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            editMode="row"
+            rowModesModel={rowModesModel}
+            onRowModesModelChange={handleRowModesModelChange}
+            onRowEditStop={handleRowEditStop}
+            processRowUpdate={processRowUpdate}
+            slotProps={{
+              toolbar: { setRows, setRowModesModel },
+            }}
+            pageSizeOptions={20}
+            rowsPerPageOptions={[20]}
+            sx={{ color: "text.light", width: 1376, overflow: "hidden" }}
+          />
+        </Box>
+      </Box>
+    </>
   );
 }
 
