a6a45dba49795297ab9dbf40953a0f9b98ad8d0d
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-08-09 20:18:15 +0700

    [EPM-34][LoiLT] fix: fix re-render logic

--- src/pages/FinalPointView/hooks/useCalculateData.js
+++ src/pages/FinalPointView/hooks/useCalculateData.js
@@ -1,11 +1,11 @@
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext } from "react";
 import { FinalPointContext } from "../context/finalPointContext";
 import { API_ENDPOINTS } from "../../../utils/api";
-import { clientDataFormatter } from "../dataFormatter";
 import { toastError, toastSuccess } from "../../../utils/toast";
 import useAuth from "../../../hooks/useAuth";
 import useDebounce from "../../../hooks/useDebounce";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import useFetchStudentData from "./useFetchStudentData";
 const useCalculateData = () => {
   const { setRows, setOriginalRows, selectedSemester, selectedYear } =
     useContext(FinalPointContext);
@@ -13,26 +13,19 @@ const useCalculateData = () => {
   const {
     auth: { accessToken },
   } = useAuth();
-  const [isLoading, setIsLoading] = useState(false);
+
+  const { isLoading, setIsLoading, fetchData } = useFetchStudentData();
   const calculateData = useCallback(async () => {
     if (selectedSemester && selectedYear) {
       setIsLoading(true);
       try {
-        const response = await axios.post(
+        await axios.post(
           `${API_ENDPOINTS.FINAL_POINTS.ADD}/${selectedYear}&${selectedSemester}`,
           {
             headers: { Authorization: `Bearer ${accessToken}` },
           }
         );
-        const data = response.data.data || [];
-        const formattedData = data.map((item, index) => ({
-          ...item,
-          id: index + 1,
-          ...clientDataFormatter(item),
-        }));
-
-        setRows(formattedData);
-        setOriginalRows(formattedData);
+        fetchData();
         toastSuccess("Calculate successfully.");
       } catch (error) {
         toastError("Calculate fail.");
