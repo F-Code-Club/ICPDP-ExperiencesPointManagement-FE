588aabea87d6cb088587519aaa2dd93329655fe2
Quoc Huy <huy.nguyen@omnibox.com.vn>
2024-08-12 21:35:10 +0700

    [HuyNBQ] refactor: use temp endpoint

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_URL } from "../index";
 
-const TEMP_API = ""
+const TEMP_API = "https://epm-be-dev.f-code.tech";
 export default axios.create({
   baseURL: TEMP_API || API_URL,
   headers: {
